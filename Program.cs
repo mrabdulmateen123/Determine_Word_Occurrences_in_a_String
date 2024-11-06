c67a785cd3d5e87d04dde6298506ae24b9a089d2
Abdul Mateen Siddiqui <mr.abdulmateen123@gmail.com>
2024-11-06 23:40:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Determine_Word_Occurrences_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Below are actual data thats given in asiignmnet 2 "Determine Word Occurrences in a String"
+            string[] myWords = { "My", "NEW", "Test", "Fix", "cup", "Car" };
+            string longString =@"jsdfkfjslffTESTrtyty4my73589347yeqw672345[l;fxnnadfsleqwrewurmyydsfTesthcverTetswewyrklsdg9i
+                               erhdsfixjhfewycupriuewyqodhfsooiemywryuhdflyaewirhdfliuycupehivjlfixyeocnFIXyiernewpanvlhdtwu
+                               terga[] dsfu96456834256new896jksdnvgFixaesi0erw0car - eryn7h467newhn6mn78k89m,9 < (< 0.<
+                               ht6";
+            // convert in lowercase without using buld in function
+            string longString_lowerCase = converInlowerCase(longString);
+           
+            //Count occurrences of each word from the long string
+            foreach (string word in myWords)
+            {
+                
+                string[] result = longString_lowerCase.Split(new string[] { converInlowerCase(word) }, StringSplitOptions.None);
+                int count = result.Length - 1;
+                Console.WriteLine($"The word '{word}' occurred {count} times.");
+            }
+
+            Console.ReadLine();
+        }
+        // Function to convert to lowercase without using built-in functions.
+        static string converInlowerCase(string input)
+        {
+            char[] chars = input.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] >= 'A' && chars[i] <= 'Z')
+                {
+                    chars[i] = (char)(chars[i] + 32); // Adding 32 to its ASCII value for converting lowercase
+                }
+            }
+            return new string(chars);
+        }
+
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Determine_Word_Occurrences_in_a_String")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Determine_Word_Occurrences_in_a_String")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36f6502a-112c-4ab6-9280-d02f5d3c8356")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
